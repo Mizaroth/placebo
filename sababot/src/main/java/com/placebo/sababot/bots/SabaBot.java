90609b2484ce63aefded08a8971a6ad678f4ff71
Carmine <carmine.barbato@outlook.com>
2018-03-26 21:41:17 +0200

    Add Sababot class,Main implementation

---
+++ sababot/src/main/java/com/placebo/sababot/bots/SabaBot.java
@@ -0,0 +1,24 @@
+package com.placebo.sababot.bots;
+
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class SabaBot extends TelegramLongPollingBot {
+
+	public String getBotUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void onUpdateReceived(Update arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getBotToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
