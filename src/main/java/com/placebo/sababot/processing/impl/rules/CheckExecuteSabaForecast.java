8a876f6eae7053d928cfaae30bbe1075802d995e
Mizaroth <ahah93@yahoo.it>
2018-04-03 20:37:14 +0200

    Closes #18 - Implemented RESTful API call for Weather calculation and basic reaction.

---
+++ src/main/java/com/placebo/sababot/processing/impl/rules/CheckExecuteSabaForecast.java
@@ -0,0 +1,28 @@
+package com.placebo.sababot.processing.impl.rules;
+
+import org.telegram.telegrambots.api.objects.Message;
+
+import com.placebo.sababot.constants.ReactionConstants;
+import com.placebo.sababot.models.UpdateReceivedContext;
+import com.placebo.sababot.processing.api.CheckRule;
+
+public class CheckExecuteSabaForecast implements CheckRule {
+
+  @Override
+  public <T extends UpdateReceivedContext> boolean check(T updateContext) {
+    Message message = updateContext.getUpdate().getMessage();
+    String messageCapitalized = message.getText() != null ? message.getText().toUpperCase() : null;
+
+    return messageCapitalized != null 
+        && containsOneOf(messageCapitalized, ReactionConstants.getSabaForecast());
+  }
+  
+  private boolean containsOneOf(String message, String[] triggers) {
+    for(String trigger : triggers) {
+      if(message.matches(".*\\b" + trigger + "\\b.*"))
+        return true;
+    }
+    return false;
+  }
+
+}
