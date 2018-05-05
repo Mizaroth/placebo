0142e06c09a36d400a8c7c827c8ea98bba3693af
Mizaroth <ahah93@yahoo.it>
2018-05-05 14:43:30 +0200

    Closes #21

--- src/main/java/com/placebo/sababot/repository/dao/TriggerDAO.java
+++ src/main/java/com/placebo/sababot/repository/dao/TriggerDAO.java
@@ -1,14 +1,33 @@
 package com.placebo.sababot.repository.dao;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.placebo.sababot.models.Trigger;
 import com.placebo.sababot.repository.api.AbstractGenericDAO;
 
 @Repository
+@Transactional
 public class TriggerDAO extends AbstractGenericDAO<Trigger> {
+  private String needsToBeCleanedQuery;
+  private String performCleaningStatement;
   public TriggerDAO() {
     super();
     setClazz(Trigger.class);
+  }
+
+  public Boolean needsToBeCleaned() {
+    return (Boolean) getCurrentSession().getNamedNativeQuery(needsToBeCleanedQuery).uniqueResult();
+  }
+  
+  public int performCleaning() {
+    return getCurrentSession().getNamedNativeQuery(performCleaningStatement).executeUpdate();
+  }
+  
+  public void setNeedsToBeCleanedQuery(String needsToBeCleanedQuery) {
+    this.needsToBeCleanedQuery = needsToBeCleanedQuery;
+  }
+  public void setPerformCleaningStatement(String performCleaningStatement) {
+    this.performCleaningStatement = performCleaningStatement;
   }
 }
